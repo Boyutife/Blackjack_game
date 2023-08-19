69297a1d64a10e65a5363517ebf33803ec6d1936
iamBoluwatife <boluwatifeaiyenibe@gmail.com>
2023-08-19 16:36:07 +0100

    Display sum and cards

--- index.js
+++ index.js
@@ -7,8 +7,12 @@ let isAlive = true;
 let message = "";
 
 let messageEl = document.getElementById('message-el');
+let sumEl = document.getElementById('sum-el');
+let cardsEl = document.getElementById('cards-el');
 
 function startGame() {
+  sumEl.textContent = "Sum: " + sum;
+  cardsEl.textContent = "Cards: " + firstCard + " " + SecondCard
   if (sum <= 20) {
     message = "Do you want to draw a new card?"
   } else if (sum === 21) {
@@ -18,7 +22,6 @@ function startGame() {
     message = "You're out of the game!"
     isAlive = false;
   }
-
   messageEl.textContent = message;
 }
 
