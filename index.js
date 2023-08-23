0783b455b22463846fc64d01c1c15e76b8f3cb8f
iamBoluwatife <boluwatifeaiyenibe@gmail.com>
2023-08-23 09:16:15 +0100

    added the for loop to display the cards

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let firstCard = 7;
 let SecondCard = 8;
+let cards = [firstCard, SecondCard]
 let sum = firstCard + SecondCard
 // Introduce a variable that keeps the state of the game
 let hasBlackJack = false;
@@ -10,10 +11,16 @@ let messageEl = document.getElementById('message-el');
 let sumEl = document.getElementById('sum-el');
 let cardsEl = document.getElementById('cards-el');
 
-
 function startGame() {
+  renderGame()
+}
+
+function renderGame() {
   sumEl.textContent = "Sum: " + sum;
-  cardsEl.textContent = "Cards: " + firstCard + " " + SecondCard
+  cardsEl.textContent = "Cards: "
+  for (let i = 0; i < cards.length; i++) {
+    cardsEl.textContent += cards[i] + " "
+  }
   if (sum <= 20) {
     message = "Do you want to draw a new card?"
   } else if (sum === 21) {
@@ -29,6 +36,7 @@ function startGame() {
 function newCard() {
   let card = 2;
   sum += card
-  startGame()
+  cards.push(card)
+  renderGame()
 }
 
